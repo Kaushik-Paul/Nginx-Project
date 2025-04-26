57568312795fe6aaa99af6a095e77d99250ffc9d
kaushik <kaushik.paul755@gmail.com>
2025-04-26 15:07:25 +0530

    Add Dockerfile

--- com.example/app/Main.java
+++ com.example/app/Main.java
@@ -2,12 +2,14 @@ package app;
 
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/", new GetHandler("Hello World from Nginx"));
+        String hostname = InetAddress.getLocalHost().getHostName();
+        server.createContext("/", new GetHandler("Hello World from " + hostname));
         server.setExecutor(null); // creates a default executor
         server.start();
         System.out.println("Server started at http://localhost:8000/hello");
